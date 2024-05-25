915c8f9eb045c8bd895168321b23d4088aa28e62
Dave Zadok <david@dzadok.dev>
2024-05-25 17:35:17 -0400

    Update formatters

--- init.lua
+++ init.lua
@@ -665,6 +665,8 @@ require('lazy').setup({
         -- You can use a sub-list to tell conform to run *until* a formatter
         -- is found.
         javascript = { { 'prettierd', 'prettier' } },
+        typescript = { { 'prettierd', 'prettier' } },
+        go = { 'gofmt' },
       },
     },
   },
