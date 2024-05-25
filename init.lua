d8dc27bda615aaea1f512f1001e267e91a742575
Dave Zadok <david@dzadok.dev>
2024-05-25 17:35:17 -0400

    Update formatters

--- init.lua
+++ init.lua
@@ -719,8 +719,9 @@ require('lazy').setup({
         -- Conform can also run multiple formatters sequentially
         -- python = { "isort", "black" },
         --
-        -- You can use 'stop_after_first' to run the first available formatter from the list
-        -- javascript = { "prettierd", "prettier", stop_after_first = true },
+        javascript = { { 'prettierd', 'prettier' } },
+        typescript = { { 'prettierd', 'prettier' } },
+        go = { 'gofmt' },
       },
     },
   },
