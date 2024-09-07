de541dcc68869b3a55bbee8858183566a88be784
Dave Zadok <david@dzadok.dev>
2024-09-07 14:41:20 -0400

    Update

--- init.lua
+++ init.lua
@@ -504,9 +504,8 @@ require('lazy').setup({
     'neovim/nvim-lspconfig',
     dependencies = {
       -- Automatically install LSPs and related tools to stdpath for Neovim
-      -- Mason must be loaded before its dependents so we need to set it up here.
-      -- NOTE: `opts = {}` is the same as calling `require('mason').setup({})`
-      { 'williamboman/mason.nvim', opts = {} },
+      { 'williamboman/mason.nvim', config = true }, -- NOTE: Must be loaded before dependants
+      'scalameta/nvim-metals',
       'williamboman/mason-lspconfig.nvim',
       'WhoIsSethDaniel/mason-tool-installer.nvim',
 
@@ -759,6 +758,7 @@ require('lazy').setup({
         },
       }
     end,
+    opts = { setup = { metals = {} } },
   },
 
   { -- Autoformat
